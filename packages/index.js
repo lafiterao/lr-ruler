f024436663e6953bc85d39bdc20bdfcf003aa0ad
lafite.rao <827943653@qq.com>
2022-11-30 00:04:03 +0800

    fix: import

--- packages/index.js
+++ packages/index.js
@@ -2,7 +2,7 @@
  * @Author: lafite.rao 827943653@qq.com
  * @Date: 2022-11-28 23:09:14
  * @LastEditors: lafite.rao 827943653@qq.com
- * @LastEditTime: 2022-11-28 23:23:58
+ * @LastEditTime: 2022-11-29 23:50:34
  * @FilePath: \lr-ruler\packages\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,6 @@ const allJs = require.context('./', true, /\.js$/)
 
 const allInstalls = (Vue)=>{
     allJs.keys().forEach(item=>{
-        console.log(allJs(item), 'allJs(item)>>>');
         if(allJs(item).default && allJs(item).default.name && allJs(item).default.name !== 'allInstalls') {
             Vue.use(allJs(item).default)
         }
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: lafite.rao 827943653@qq.com
+ * @Date: 2022-11-28 22:57:29
+ * @LastEditors: lafite.rao 827943653@qq.com
+ * @LastEditTime: 2022-11-29 23:47:47
+ * @FilePath: \lr-ruler\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div id="app">
       <lr-ruler
@@ -13,10 +21,10 @@
 </template>
 
 <script>
-import LrRuler from 'lr-ruler';
+// import LrRuler from 'lr-ruler';
 export default {
   name: 'App',
-  components: { LrRuler },
+  // components: { LrRuler },
   methods: {
     handleWeight(msg) {
       console.log(msg)//该值打印的就是你改变刻度尺的值
