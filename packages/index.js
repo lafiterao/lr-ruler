3d0d452b0943825d0041c52f4850bcff6e0ce1fc
lafite.rao <827943653@qq.com>
2022-11-30 00:49:54 +0800

    refrector: 抛弃context

--- packages/index.js
+++ packages/index.js
@@ -2,20 +2,26 @@
  * @Author: lafite.rao 827943653@qq.com
  * @Date: 2022-11-28 23:09:14
  * @LastEditors: lafite.rao 827943653@qq.com
- * @LastEditTime: 2022-11-29 23:50:34
+ * @LastEditTime: 2022-11-30 00:45:36
  * @FilePath: \lr-ruler\packages\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-const allJs = require.context('./', true, /\.js$/)
 
-const allInstalls = (Vue)=>{
-    allJs.keys().forEach(item=>{
-        if(allJs(item).default && allJs(item).default.name && allJs(item).default.name !== 'allInstalls') {
-            Vue.use(allJs(item).default)
-        }
+import lrRuler from "./lr-ruler";
+const components = [lrRuler]
+const install = function (Vue) {
+    // 遍历注册所有的组件
+    components.map(com=>{
+      Vue.component(com.name,com)
     })
+  }
+// 注意这里的判断，很重要
+if(typeof window !== 'undefined' && window.Vue) {
+    install(window.Vue)
 }
-
-console.log(allInstalls, 'allInstalls>>>>');
-export default allInstalls
+export default {
+    install,
+    // 组件列表
+    ...components
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: lafite.rao 827943653@qq.com
  * @Date: 2022-11-28 22:57:29
  * @LastEditors: lafite.rao 827943653@qq.com
- * @LastEditTime: 2022-11-29 23:48:11
+ * @LastEditTime: 2022-11-30 00:38:43
  * @FilePath: \lr-ruler\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
