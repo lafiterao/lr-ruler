d4b74ab50acdcc1e43809f2bc924db47b50395fe
lafite.rao <827943653@qq.com>
2022-11-30 01:44:59 +0800

    fix: console

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: lafite.rao 827943653@qq.com
  * @Date: 2022-11-28 22:57:29
  * @LastEditors: lafite.rao 827943653@qq.com
- * @LastEditTime: 2022-11-30 01:42:45
+ * @LastEditTime: 2022-11-30 01:44:26
  * @FilePath: \lr-ruler\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -24,7 +24,9 @@
 export default {
   name: 'App',
   methods: {
-    handleWeight(msg) {}
+    handleWeight() {
+      // console.log(msg)//该值打印的就是你改变刻度尺的值
+    }
   }
 }
 </script>
